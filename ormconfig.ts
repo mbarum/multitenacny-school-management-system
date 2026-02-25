c1fe42d467e4266cb2aaefdebc0f787b74b34841
mbarum <mutheeisaiah9@gmail.com>
2026-02-25 16:30:14 +0300

    feat: Provide default DB connection values
    Set default values for DB host and port in ormconfig.ts to 'localhost' and '3306' respectively. This simplifies local development and testing by removing the need to explicitly set these environment variables.

--- ormconfig.ts
+++ ormconfig.ts
@@ -5,8 +5,8 @@ config();
 
 export default new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
